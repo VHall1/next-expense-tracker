b0923ea827aa5eba223bb65e67229b111e5ef78a
VHall1 <victorhallpsn@hotmail.com>
2023-08-15 21:56:54 +0100

    add navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,14 @@
+export const Navbar = () => {
+  return (
+    <header className="fixed w-screen">
+      <div className="max-w-screen-lg mx-auto px-4 h-14 flex items-center">
+        <div className="mr-4">CoolLogo</div>
+        <a className="mr-4">link a</a>
+        <a className="mr-4">link a</a>
+        <a>link b</a>
+
+        <a className="ml-auto">Account</a>
+      </div>
+    </header>
+  );
+};
