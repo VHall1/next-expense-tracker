78903ad0b9f380224908c3f7f7f41bea59339019
VHall1 <victorhallpsn@hotmail.com>
2023-08-17 20:50:20 +0100

    display current user

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,29 @@
-export const Navbar = () => {
+import { decodeJWT } from "@/utils/authenticate";
+import { prisma } from "@/utils/prisma";
+import { cookies } from "next/headers";
+
+const getCurrentUser = () => {
+  const cookiesList = cookies();
+  const cookie = cookiesList.get("uid");
+  if (!cookie) {
+    return null;
+  }
+
+  const userID = decodeJWT(cookie.value);
+  if (!userID) {
+    return null;
+  }
+
+  return prisma.user.findUnique({
+    where: {
+      id: userID,
+    },
+  });
+};
+
+export const Navbar = async () => {
+  const user = await getCurrentUser();
+
   return (
     <header className="fixed w-screen">
       <div className="max-w-screen-lg mx-auto px-4 h-14 flex items-center">
@@ -7,7 +32,9 @@ export const Navbar = () => {
         <a className="mr-4">link a</a>
         <a>link b</a>
 
-        <a className="ml-auto">Account</a>
+        <div className="ml-auto">
+          {user ? <a>{user.email}</a> : <a>Sign Up</a>}
+        </div>
       </div>
     </header>
   );
