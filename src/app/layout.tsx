7b128fcf36a753ab9691caada9b8f8e4ff36993d
VHall1 <victorhallpsn@hotmail.com>
2023-08-23 14:27:33 +0100

    move navbar to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/components/providers";
+import { Navbar } from "@/components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,10 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>{children}</Providers>
+        <Providers>
+          <Navbar />
+          {children}
+        </Providers>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,29 +7,26 @@ export default async function Dashboard() {
   const transactions = await getTransactions();
 
   return (
-    <>
-      <Navbar />
-      <main className="max-w-screen-lg mx-auto px-4 pt-24">
-        <div className="flex justify-between">
-          <h3 className="text-3xl font-medium">Transactions</h3>
-          <Button variant="solid">Add transaction</Button>
-        </div>
+    <main className="max-w-screen-lg mx-auto px-4 pt-24">
+      <div className="flex justify-between">
+        <h3 className="text-3xl font-medium">Transactions</h3>
+        <Button variant="solid">Add transaction</Button>
+      </div>
 
-        <Transactions
-          transactions={transactions.map((transaction) => ({
-            id: transaction.id,
-            description: transaction.description,
-            amount: Intl.NumberFormat("en-GB", {
-              style: "currency",
-              currency: "GBP",
-            }).format(transaction.amount.toNumber()),
-            timestamp: new Intl.DateTimeFormat("en-GB").format(
-              transaction.timestamp
-            ),
-          }))}
-        />
-      </main>
-    </>
+      <Transactions
+        transactions={transactions.map((transaction) => ({
+          id: transaction.id,
+          description: transaction.description,
+          amount: Intl.NumberFormat("en-GB", {
+            style: "currency",
+            currency: "GBP",
+          }).format(transaction.amount.toNumber()),
+          timestamp: new Intl.DateTimeFormat("en-GB").format(
+            transaction.timestamp
+          ),
+        }))}
+      />
+    </main>
   );
 }
 
